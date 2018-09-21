437373760c1f5c01205e9753c45377096393c5aa
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-09-20 20:58:31 -0400

    Make ISPC energy thread-safe + speed up distance query with lazy evaluation.

--- src/tetwild/MeshRefinement.cpp
+++ src/tetwild/MeshRefinement.cpp
@@ -45,7 +45,7 @@ void MeshRefinement::prepareData(bool is_init) {
     getSimpleMesh(simple_mesh);
     GEO::MeshFacetsAABB simple_tree(simple_mesh);
     LocalOperations localOperation(tet_vertices, tets, is_surface_fs, v_is_removed, t_is_removed, tet_qualities,
-                                   state.ENERGY_AMIPS, simple_tree, simple_tree, args, state);
+                                   state.ENERGY_AMIPS, simple_mesh, simple_tree, simple_tree, args, state);
     localOperation.calTetQualities(tets, tet_qualities, true);//cal all measure
     double tmp_time = igl_timer.getElapsedTime();
     logger().debug("{}s", tmp_time);
@@ -217,7 +217,7 @@ void MeshRefinement::refine(int energy_type, const std::array<bool, 4>& ops, boo
         min_adaptive_scale = (state.bbox_diag / 1000) / state.initial_edge_len; // set min_edge_length to diag / 1000 would be better
 
     LocalOperations localOperation(tet_vertices, tets, is_surface_fs, v_is_removed, t_is_removed, tet_qualities,
-                                   energy_type, geo_sf_tree, geo_b_tree, args, state);
+                                   energy_type, geo_sf_mesh, geo_sf_tree, geo_b_tree, args, state);
     EdgeSplitter splitter(localOperation, state.initial_edge_len * (4.0 / 3.0) * state.initial_edge_len * (4.0 / 3.0));
     EdgeCollapser collapser(localOperation, state.initial_edge_len * (4.0 / 5.0) * state.initial_edge_len * (4.0 / 5.0));
     EdgeRemover edge_remover(localOperation, state.initial_edge_len * (4.0 / 3.0) * state.initial_edge_len * (4.0 / 3.0));
